9b24a0635ff63857d35b08932d6eb64583ceb356
A S M Abir <75290115+mrasmabir@users.noreply.github.com>
2024-08-03 00:18:30 -0600

    Files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import AppRouter from './AppRouter';
 
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,13 +4,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ForgotPassword from './pages/ForgotPassword';
-import Home from './pages/Home';
 import Profile from './pages/Profile';
-
+import CardDetails from './pages/CardDetails';
 import Nav from './components/Nav';
 import Dashboard from './pages/Dashboard';
 import PostProblem from './pages/PostProblem';
-import PostSolution from './pages/PostSolution';
+
 import Results from './pages/Results';
 import About from './pages/About';
 
@@ -23,14 +22,15 @@ const AppRouter = () => {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/home" element={<Home />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/results" element={<Results />} />
+                <Route path='/' element={<Results />} />
+                <Route path='/details/:id' element={<CardDetails />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/forgotPassword" element={<ForgotPassword />} />
                 <Route path="/postProblem" element={<PostProblem />} />
-                <Route path="/postSolution" element={<PostSolution />} />
+                
 
                 <Route path="/" element={<SignIn />} />
             </Routes>
